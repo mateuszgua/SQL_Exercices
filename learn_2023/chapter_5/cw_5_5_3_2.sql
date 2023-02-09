2dbe224d725dc46d8b9ec642ae8a2428ea3671cc
Mateusz Gula <mateuszgua@wp.eu>
2023-02-09 11:28:23 +0100

    Add practise excercise 5.1 for chapter 5

--- learn_2023/chapter_5/cw_5_5_3_2.sql
+++ learn_2023/chapter_5/cw_5_5_3_2.sql
@@ -1,7 +1,3 @@
-SELECT (TS_LEXIZE('english_stem',
-				  UNNEST(STRING_TO_ARRAY(REGEXP_REPLACE(FEEDBACK, '[^a-zA-Z]+', ' ', 'g'),
-				' '))))[1] AS TOKEN, AVG(rating) as avg_rating
+SELECT *
 FROM CUSTOMER_SURVEY
-GROUP BY 1
-HAVING COUNT(1) >= 3
-ORDER BY 2;
+WHERE FEEDBACK ilike '%pop%';
